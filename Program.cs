1c71dbebdd35b545645c58aaed65e1578665b181
Uday-Kiran-S <151548069+Uday-Kiran-S@users.noreply.github.com>
2023-11-21 00:02:38 +0530

    exception

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace FirstApp
             Console.WriteLine("Hello World");
             Console.WriteLine("Varun MJ Here.....");
             Console.WriteLine("Varun MJ Here.....");
+            throw new Exception("Hello");
             Console.ReadLine();
         }
     }
