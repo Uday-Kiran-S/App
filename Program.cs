6de0f72d6294d304ebaa8ec5ccb723d35831a308
Uday-Kiran-S <151548069+Uday-Kiran-S@users.noreply.github.com>
2023-11-20 23:24:15 +0530

    Changed

--- Program.cs
+++ Program.cs
@@ -8,10 +8,6 @@ namespace FirstApp
             Console.WriteLine("Hello World");
             Console.WriteLine("Varun MJ Here.....");
             Console.WriteLine("Varun MJ Here.....");
-            Console.WriteLine("Varun MJ Here.....");
-            Console.WriteLine("Varun MJ Here.....");
-            Console.WriteLine("Varun MJ Here.....");
-            Console.WriteLine("Varun MJ Here.....");
             Console.ReadLine();
         }
     }
