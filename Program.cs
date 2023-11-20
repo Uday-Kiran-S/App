706e862e0a47b4266c297ba487fba43de53b25ad
Uday-Kiran-S <151548069+Uday-Kiran-S@users.noreply.github.com>
2023-11-20 22:33:05 +0530

    Second Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FirstApp
+{
+    public class App
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
